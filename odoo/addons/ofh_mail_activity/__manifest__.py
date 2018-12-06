de287cb992ce8eb945b3068fad8a3f26d9333764
Zakaria Makrelouf <zakaria.makrelouf@tajawal.com>
2018-12-06 14:38:36 +0400

    [IMP]ofh_mail_activity: assign activity for multiple users.

---
+++ odoo/addons/ofh_mail_activity/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2018 Tajawal LLC
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Ofh Mail Activity',
+    'description': """
+        Assign activity to list of users instead of only one user""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Tajawal LLC',
+    'website': 'https://tajawal.com',
+    'depends': [
+        'mail',
+    ],
+    'data': [
+        'templates/assets.xml',
+    ],
+    'demo': [
+    ],
+}
