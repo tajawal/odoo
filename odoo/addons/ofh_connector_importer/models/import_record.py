c4ff8b1f8305d8da62a6c7d286ad563e5687ad51
Zakaria Makrelouf <zakaria.makrelouf@tajawal.com>
2019-01-23 16:27:54 +0400

    [IMP]ofh_connector_importer generic module for connector importer.

---
+++ odoo/addons/ofh_connector_importer/models/import_record.py
@@ -0,0 +1,35 @@
+# Copyright 2019 Tajwal LLC
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class ImportRecord(models.Model):
+
+    _inherit = 'import.record'
+
+    @api.multi
+    def _import_report(self, import_type, file_name, data):
+        """Import a given data to the system.
+
+        :param import_type: import.type record
+        :type import_type: import.type object
+        :param file_name: The name of the file to be imported.
+        :type file_name: str
+        :param data: file like object mainly
+        :type data: binary
+        """
+
+        self.ensure_one()
+        source = self.env['import.source.csv'].create({
+            'csv_file': data,
+            'csv_filename': file_name,
+            'csv_delimiter': ','})
+
+        recordset = self.env['import.recordset'].create({
+            'backend_id': self.id,
+            'import_type_id': import_type.id,
+            'source_id': source.id,
+            'source_model': 'import.source.csv',
+        })
+        return recordset.run_import()
