2b424ed43943e2d87ee197b1b0fe62d5bcd6e7a4
Zakaria Makrelouf <zakaria.makrelouf@tajawal.com>
2019-02-05 10:59:56 +0400

    Ayesha Bump version: 1.3.6 -> 1.4.0

--- odoo/addons/ofh_all/__manifest__.py
+++ odoo/addons/ofh_all/__manifest__.py
@@ -7,7 +7,7 @@
     'author': 'ACSONE SA/NV',
     'website': "http://acsone.eu",
     'category': 'odoo_finance_hub',
-    'version': '11.0.1.3.6',
+    'version': '11.0.1.4.0',
     'license': 'AGPL-3',
     'depends': [
         # odoo_finance_hub open source addons
