fd5b6e9819e9795de2326144b50a3ab029602b05
Zakaria Makrelouf <zakaria.makrelouf@tajawal.com>
2019-05-06 11:41:06 +0400

    [IMP]ofh_sale_order_supplier_invoice: GDS office and command_cryptic access rights.

--- odoo/addons/ofh_sale_order_supplier_invoice/__manifest__.py
+++ odoo/addons/ofh_sale_order_supplier_invoice/__manifest__.py
@@ -15,6 +15,7 @@
     'data': [
         'security/connector_importer.xml',
         'security/ofh_supplier_invoice.xml',
+        'security/ofh_gds_office.xml',
         'wizards/ofh_sale_order_line_investigation_tag.xml',
         'wizards/ofh_supplier_invoice_line_investigation_tag.xml',
         'wizards/ofh_payment_request_not_applicable.xml',
