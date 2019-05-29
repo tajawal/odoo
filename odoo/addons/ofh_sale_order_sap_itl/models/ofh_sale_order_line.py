33c1d9e51873f8b2c6d7b77c571aacfbe8f1ca8a
Zakaria Makrelouf <zakaria.makrelouf@tajawal.com>
2019-05-29 04:24:05 +0400

    [FIX]ofh_sale_order_sap_itl: ITL use total amount for cost not net.

--- odoo/addons/ofh_sale_order_sap_itl/models/ofh_sale_order_line.py
+++ odoo/addons/ofh_sale_order_sap_itl/models/ofh_sale_order_line.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Tajawal LLC
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import api, models
 
 
 class OfhSaleOrderLine(models.Model):
@@ -14,27 +14,27 @@ class OfhSaleOrderLine(models.Model):
         lines = []
 
         total_cost = sum(
-            [abs(round(l.gds_net_amount, 2)) for l in self.invoice_line_ids])
+            [abs(round(l.total, 2)) for l in self.invoice_line_ids])
 
         for line in self.invoice_line_ids:
             line_dict = self._get_sale_line_dict()
             line_dict['pnr'] = line.locator
-            line_dict['cost_price'] = abs(line.currency_id.round(
-                line.gds_net_amount))
-            prorated_sale = \
-                (line.gds_net_amount * self.total_amount) / total_cost
+            line_dict['cost_price'] = abs(line.currency_id.round(line.total))
+
+            prorated_sale = (line.total * self.total_amount) / total_cost
             line_dict['sale_price'] = abs(
                 line.currency_id.round(prorated_sale))
 
             prorated_discount = \
-                (line.gds_net_amount * self.discount_amount) / total_cost
+                (line.total * self.discount_amount) / total_cost
             line_dict['discount'] = abs(
                 line.currency_id.round(prorated_discount))
 
-            prorated_tax = \
-                (line.gds_net_amount * self.tax_amount) / total_cost
+            prorated_tax = (line.total * self.tax_amount) / total_cost
             line_dict['output_vat'] = abs(line.currency_id.round(prorated_tax))
             line_dict['cost_currency'] = line.currency_id.name
             line_dict['ticket_number'] = line.ticket_number
+
             lines.append(line_dict)
+
         return lines
