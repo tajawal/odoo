b59cbdd279f51340ad5a49b6b0ee8642cf984b95
Zakaria Makrelouf <zakaria.makrelouf@tajawal.com>
2019-08-13 09:12:55 +0100

    [REF] increase cpu time limit.

--- infra/start.sh
+++ infra/start.sh
@@ -8,8 +8,8 @@ server_wide_modules=web,queue_job,logging_json
 limit_memory_hard = 1677721600
 limit_memory_soft = 629145600
 limit_request = 8192
-limit_time_cpu = 600
-limit_time_real = 1200
+limit_time_cpu = 1200
+limit_time_real = 1500
 max_cron_threads = 1
 log_level=debug_rpc
 
