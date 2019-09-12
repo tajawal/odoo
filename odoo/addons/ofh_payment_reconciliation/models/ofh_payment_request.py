39d6689bd1aad711e22ac1d97d260eaacfa31d2a
Muhammad Junaid <muhammad.junaid@tajawal.com>
2019-09-12 17:14:54 +0400

    Updates

--- odoo/addons/ofh_payment_reconciliation/models/ofh_payment_request.py
+++ odoo/addons/ofh_payment_reconciliation/models/ofh_payment_request.py
@@ -4,12 +4,8 @@ from odoo import fields, models
 class OfhPaymentRequest(models.Model):
     _inherit = 'ofh.payment.request'
 
-    payment_gateway_id = fields.Many2one(
+    payment_gateway_ids = fields.One2many(
         string="Payment Gateway ID",
-        required=False,
-        readonly=True,
-        index=True,
         comodel_name='ofh.payment.gateway',
-        ondelete='cascade',
-        auto_join=True,
+        inverse_name='hub_payment_id',
     )
