3953e0b5783bc1c8e4cc45b03a00c215f8de5f36
Muhammad Junaid <muhammad.junaid@tajawal.com>
2019-09-24 12:58:26 +0400

    Updates

--- odoo/addons/ofh_payment_reconciliation/models/ofh_payment.py
+++ odoo/addons/ofh_payment_reconciliation/models/ofh_payment.py
@@ -28,4 +28,54 @@ class OfhPayment(models.Model):
     is_apple_pay = fields.Boolean(
         related="payment_gateway_ids.is_apple_pay",
     )
+    matching_status = fields.Selection(
+        string="Matching Status",
+        selection=[
+            ('unmatched', 'Unmatched'),
+            ('matched', 'Matched'),
+            ('partial_matched', 'Partial Matched')],
+        default='unmatched',
+        required=True,
+        index=True,
+        readonly=True,
+        store=False,
+        compute='_compute_matching_status',
+
+    )
+    reconciliation_status = fields.Selection(
+        string="Reconciliation Status",
+        selection=[
+            ('reconciled', 'Reconciled'),
+            ('unreconciled', 'Unreconciled'),
+        ],
+        default='unreconciled',
+        compute='_compute_reconciliation_status',
+        store=False,
+        index=True,
+        readonly=True,
+    )
+
+    @api.multi
+    @api.depends(
+        'payment_gateway_ids', 'bank_settlement_ids')
+    def _compute_matching_status(self):
+        for rec in self:
+            rec.matching_status = "unmatched"
+            if rec.payment_gateway_ids and rec.bank_settlement_ids:
+                rec.matching_status = 'matched'
+                continue
+
+            if rec.payment_gateway_ids or rec.bank_settlement_ids:
+                rec.matching_status = 'partial_matched'
+                continue
+
+    @api.multi
+    @api.depends('bank_settlement_ids')
+    def _compute_reconciliation_status(self):
+        for rec in self:
+            rec.reconciliation_status = 'unreconciled'
+            if rec.bank_settlement_ids and rec.bank_settlement_ids.reconciliation_status == "reconciled":
+                rec.reconciliation_status = 'reconciled'
+                continue
+
 
