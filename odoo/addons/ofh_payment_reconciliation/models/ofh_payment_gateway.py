a4180c873ae20bd3c3484878b727064fe6e58cee
Zakaria Makrelouf <zakaria.makrelouf@tajawal.com>
2019-10-07 18:02:42 +0400

    [REF]ofh_payment_reconciliation: fix pg reconciliation.

--- odoo/addons/ofh_payment_reconciliation/models/ofh_payment_gateway.py
+++ odoo/addons/ofh_payment_reconciliation/models/ofh_payment_gateway.py
@@ -1,26 +1,29 @@
 # Copyright 2019 Tajawal LCC
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models, api, _
+from odoo import api, fields, models
 
 
 class OfhPaymentGateway(models.Model):
     _inherit = 'ofh.payment.gateway'
 
     hub_payment_id = fields.Many2one(
-        string="Hub Payment Id",
+        string="Hub Payment",
         comodel_name='ofh.payment',
         required=False,
-        ondelete='cascade'
+        ondelete="set null",
+        readonly=True,
+        track_visibility='onchange',
     )
     hub_payment_request_id = fields.Many2one(
-        string="Hub Payment Request Id",
+        string="Hub Payment Request",
         comodel_name='ofh.payment.request',
         required=False,
-        ondelete='cascade'
+        ondelete="set null",
+        readonly=True,
+        track_visibility='onchange',
     )
     hub_matching_status = fields.Selection(
-        string="Hub Matching Status",
         selection=[
             ('unmatched', 'Unmatched'),
             ('matched', 'Matched'),
@@ -29,10 +32,9 @@ class OfhPaymentGateway(models.Model):
         required=True,
         index=True,
         readonly=True,
-        track_visibility='always',
+        track_visibility='onchange',
     )
     settlement_matching_status = fields.Selection(
-        string="Settlement Matching Status",
         selection=[
             ('unmatched', 'Unmatched'),
             ('matched', 'Matched'),
@@ -41,13 +43,14 @@ class OfhPaymentGateway(models.Model):
         required=True,
         index=True,
         readonly=True,
-        track_visibility='always',
+        track_visibility='onchange',
     )
     bank_settlement_id = fields.Many2one(
         string="Bank Settlement",
         comodel_name='ofh.bank.settlement',
         required=False,
         track_visibility='onchange',
+        readonly=True,
     )
     reconciliation_status = fields.Selection(
         string="Reconciliation Status",
@@ -92,25 +95,27 @@ class OfhPaymentGateway(models.Model):
                 rec.reconciliation_status = 'not_applicable'
                 continue
 
-            rec.reconciliation_amount = \
-                abs(rec.total - rec.bank_settlement_id.gross_amount)
-
             if rec.reconciliation_tag:
                 rec.reconciliation_status = 'reconciled'
                 continue
 
+            rec.reconciliation_amount = \
+                abs(rec.total - rec.bank_settlement_id.gross_amount)
+
             if rec.reconciliation_amount <= 1:
                 rec.reconciliation_status = 'reconciled'
-            else:
-                rec.reconciliation_status = 'unreconciled'
+                continue
 
     @api.multi
     def match_with_payment(self):
         """Match a payment gateway object with a Payment or Payment Request."""
         self.ensure_one()
-        if self.hub_matching_status not in ('matched', 'not_applicable'):
-            self._match_with_payment()
+        if self.hub_matching_status in ('matched', 'not_applicable'):
+            return
+        self._match_with_payment()
+        if not self.payment_id:
             self._match_with_payment_request()
+        return
 
     @api.multi
     def _match_with_payment(self):
@@ -119,16 +124,20 @@ class OfhPaymentGateway(models.Model):
         payment_id = self.env['ofh.payment'].search(
             self._get_payment_domain(), limit=1)
 
-        if payment_id:
-            self.write({
-                "hub_payment_id": payment_id.id,
-                "hub_matching_status": 'matched'
-            })
-            # Updating the relation
-            payment_id.write({
-                'payment_gateway_id': self.id,
-                'pg_matching_status': 'matched',
-            })
+        if not payment_id:
+            return False
+
+        self.write({
+            "hub_payment_id": payment_id.id,
+            "hub_matching_status": 'matched'
+        })
+
+        # Updating the relation
+        payment_id.write({
+            'payment_gateway_id': self.id,
+            'pg_matching_status': 'matched',
+        })
+        return True
 
     # TODO: Need to remove this when done with Payments Object change
     @api.multi
@@ -136,30 +145,40 @@ class OfhPaymentGateway(models.Model):
         self.ensure_one()
         # Matching with Payment Request Logic
         payment_request_id = self.env['ofh.payment.request'].search(
-            self._get_payment_domain(), limit=1)
+            self._get_payment_request_domain(), limit=1)
 
-        if len(payment_request_id):
-            self.write({
-                "hub_payment_request_id": payment_request_id.id,
-                "hub_matching_status": 'matched'
-            })
-            # Updating the relation
-            payment_request_id.write({
-                'payment_gateway_id': self.id,
-                'pg_matching_status': 'matched',
-            })
+        if not payment_request_id:
+            return False
+
+        self.write({
+            "hub_payment_request_id": payment_request_id.id,
+            "hub_matching_status": 'matched'
+        })
+        # Updating the relation
+        payment_request_id.write({
+            'payment_gateway_id': self.id,
+            'pg_matching_status': 'matched',
+        })
+        return True
 
     @api.multi
     def _get_payment_domain(self):
+        self.ensure_one()
+        return [('track_id', '=', self.track_id)]
+
+    @api.multi
+    def _get_payment_request_domain(self):
+        self.ensure_one()
         return [
-            ('track_id', '=', self.track_id)]
+            '|', ('track_id', '=', self.track_id),
+            ('parent_track_id', '=', self.track_id)]
 
     def _force_match_payment(
             self, hub_payment_id=False, hub_payment_request_id=False):
 
         self.ensure_one()
         if not hub_payment_id and not hub_payment_request_id:
-            return
+            return False
 
         # Remove the current link in the invoice line.
         if self.hub_payment_id or self.hub_payment_request_id:
@@ -167,26 +186,25 @@ class OfhPaymentGateway(models.Model):
 
         if hub_payment_id:
             # Link the payment gateway with the new payment.
-            if hub_payment_id:
-                hub_payment_id.write({
-                    'payment_gateway_id': self.id,
-                    'pg_matching_status': 'matched',
-                })
-                return self.write({
-                    'hub_payment_id': hub_payment_id.id,
-                    'hub_matching_status': 'matched',
-                })
+            hub_payment_id.write({
+                'payment_gateway_id': self.id,
+                'pg_matching_status': 'matched',
+            })
+            self.write({
+                'hub_payment_id': hub_payment_id.id,
+                'hub_matching_status': 'matched',
+            })
+            return True
         else:
-            # Link the payment gateway with the new payment request.
-            if hub_payment_request_id:
-                hub_payment_request_id.write({
-                    'payment_gateway_id': self.id,
-                    'pg_matching_status': 'matched',
-                })
-                return self.write({
-                    'hub_payment_request_id': hub_payment_request_id.id,
-                    'hub_matching_status': 'matched',
-                })
+            hub_payment_request_id.write({
+                'payment_gateway_id': self.id,
+                'pg_matching_status': 'matched',
+            })
+            self.write({
+                'hub_payment_request_id': hub_payment_request_id.id,
+                'hub_matching_status': 'matched',
+            })
+            return True
 
     @api.multi
     def _unlink_payment(self):
