dd15e7bc0fef2aed7494e98dc60d79d6d98cbace
Mustafa Magdi <mustafa.magdi@tajawal.com>
2019-10-23 15:58:29 +0400

    add more replicas

--- infra/start.sh
+++ infra/start.sh
@@ -53,7 +53,7 @@ if psql -lqtA -h ${db_endpoint} --username ${db_user}| grep -q ${db_name}; then
   echo "exists"
   echo "Remove ir_attachement from DB (Hack should be removed)"
   psql -h ${db_endpoint} --username ${db_user} -d ${db_name} -c "DELETE FROM ir_attachment WHERE url LIKE '/web/content/%';"
-  odoo -c /opt/finance_hub/odoo.cfg -d $db_name -r $db_user -w $db_password --db_host $db_endpoint -u all --without-demo=WITHOUT_DEMO
+  odoo -c /opt/finance_hub/odoo.cfg -d $db_name -r $db_user -w $db_password --db_host $db_endpoint
   psql -h ${db_endpoint} --username ${db_user} -d ${db_name} -c "UPDATE queue_job SET state = 'pending' WHERE state IN ('enqueued', 'started');"
 else
   echo "do not exist"
