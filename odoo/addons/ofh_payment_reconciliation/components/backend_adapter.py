857e60683a865f2edc562115b8586c26198c90bb
Muhammad Junaid <muhammad.junaid@tajawal.com>
2019-11-18 18:58:41 +0400

    Updated UI fro loader

---
+++ odoo/addons/ofh_payment_reconciliation/components/backend_adapter.py
@@ -0,0 +1,28 @@
+# Copyright 2019 Tajawal LCC
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import requests
+from odoo.addons.component.core import AbstractComponent
+
+try:
+    from odoo.addons.server_environment import serv_config
+except ImportError:
+    logging.getLogger('odoo.module').warning(
+        'server_environment not available in addons path.')
+
+
+class SapXmlApi:
+
+    def __init__(self):
+        sap_xml_api_url = serv_config.get('sap_backend.live-sap', 'sap_xml_api_url')
+        self.sap_xml_api_url = sap_xml_api_url
+        self.headers = {
+            'Content-type': 'application/json;charset=UTF-8',
+            'user-agent': 'finance_hub_api/0.1',
+            'Accept': 'application/json',
+        }
+
+    def generate_loader(self, payload: dict) -> dict:
+        url = f"{self.sap_xml_api_url}payment_loader/generate"
+        response = requests.post(url, json=payload, headers=self.headers)
+        response.raise_for_status()
+        return response
--- odoo/addons/ofh_payment_reconciliation/wizards/ofh_payment_loader.py
+++ odoo/addons/ofh_payment_reconciliation/wizards/ofh_payment_loader.py
@@ -4,6 +4,7 @@
 
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
+from ..components.backend_adapter import SapXmlApi
 
 
 class OfhPaymentLoader(models.TransientModel):
@@ -14,16 +15,17 @@ class OfhPaymentLoader(models.TransientModel):
             ('almosafer', 'Almosafer'),
             ('tajawal', 'Tajawal')],
         default="almosafer",
+        required=True
     )
     bank_name = fields.Selection(
         string="Bank Name",
         selection=[
             ('mashreq', 'Mashreq'),
-            ('cib', 'CIB'),
             ('sabb', 'SABB'),
             ('rajhi', 'Rajhi'),
             ('knet', 'Knet'),
             ('amex', 'Amex')],
+        required=True
     )
     provider = fields.Selection(
         string="Provider",
@@ -32,17 +34,84 @@ class OfhPaymentLoader(models.TransientModel):
             ('fort', 'Fort'),
             ('knet', 'Knet'),
         ],
+        required=True
     )
     settlement_date = fields.Date(
         string="Settlement Date",
+        required=True
     )
     is_apple_pay = fields.Boolean(
         string="Apple Pay?",
         default=False,
     )
-    currency = fields.Selection(
-        string="Currency",
-        selection=[
-            ('aed', 'AED'),
-            ('sar', 'SAR')],
+    currency_id = fields.Many2one(
+        string='Currency',
+        comodel_name='res.currency',
+        required=True
     )
+
+    @api.multi
+    def generate_loader(self):
+        payments = self._get_eligible_payments()
+        if payments:
+            params = self._prepare_loader_params(payments)
+            sap_api = SapXmlApi()
+            response = sap_api.generate_loader(params)
+
+        return response
+
+    def _get_eligible_payments(self):
+        if self.is_apple_pay:
+            apply_pay_condition = f"AND pg.is_apple_pay={self.is_apple_pay}"
+        else:
+            apply_pay_condition = ""
+
+        self.env.cr.execute(f"""
+                    SELECT pg.*, 
+                           p.mid, 
+                           p.assignment       AS assignment, 
+                           bs.settlement_date AS settlement_date, 
+                           so.name            AS order_number 
+                    FROM   ofh_payment_gateway AS pg 
+                           JOIN ofh_payment AS p 
+                             ON pg.hub_payment_id = p.id 
+                           JOIN ofh_bank_settlement AS bs 
+                             ON pg.bank_settlement_id = bs.id 
+                           JOIN ofh_sale_order AS so 
+                             ON p.order_id = so.id 
+                    WHERE  pg.settlement_matching_status = 'matched' 
+                           AND pg.reconciliation_status = 'reconciled' 
+                           AND pg.entity = '{self.entity}' 
+                           AND pg.acquirer_bank = '{self.bank_name}' 
+                           AND pg.provider = '{self.provider}'
+                           {apply_pay_condition} 
+                           AND p.currency_id = {self.currency_id.id} 
+                           AND bs.settlement_date = '{self.settlement_date}' 
+                """)
+        payments = self.env.cr.dictfetchall()
+        return payments
+
+    def _prepare_loader_params(self, payments):
+        params = {
+            "entity": self.entity,
+            "bank_name": self.bank_name,
+            "payment_gateway": self.provider,
+            "settlement_date": self.settlement_date,
+            "currency": self.currency_id.name,
+            "is_apple_pay": self.is_apple_pay,
+        }
+
+        data = []
+        for payment in payments:
+            p_params = {
+                "order_number": payment["order_number"],
+                "payment_status": payment["payment_status"],
+                "total": payment["total"],
+                "assignment": payment["assignment"],
+                "auth_code": payment["auth_code"],
+                "mid": payment["mid"]
+            }
+            data.append(p_params)
+
+        params.update({"data": data})
+        return params
