ec508fbe7522dddda7b9a967417a12a68ff98206
Muhammad junaid <33692283+mjunaidtajawal@users.noreply.github.com>
2020-03-17 14:15:52 +0400

    Added timedelta library and changed cron timmings (#303)

--- odoo/addons/ofh_supplier_invoice_tv/models/ofh_supplier_invoice_line.py
+++ odoo/addons/ofh_supplier_invoice_tv/models/ofh_supplier_invoice_line.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import api, fields, models
 from odoo.addons.queue_job.job import job
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class OfhSupplierInvoiceLine(models.Model):
     _inherit = 'ofh.supplier.invoice.line'
